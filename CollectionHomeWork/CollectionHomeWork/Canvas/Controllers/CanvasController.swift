06dabad5e3540678d3cc16d4fdb2cabd064e6657
Badyi Shagaalan <badyi7@icloud.com>
2021-06-04 22:59:17 +0300

    Draw Module
    added tool collection below canvas

--- CollectionHomeWork/CollectionHomeWork/Canvas/Controllers/CanvasController.swift
+++ CollectionHomeWork/CollectionHomeWork/Canvas/Controllers/CanvasController.swift
@@ -7,10 +7,21 @@
 
 import UIKit
 
-class CanvasController : UIViewController {
+final class CanvasController : UIViewController {
     
     var tableView = ColorTableView()
     let colorButton = ColorButton()
+    
+    private lazy var toolCollection: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.register(ToolCell.self, forCellWithReuseIdentifier: ToolCell.id)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.delegate = self
+        cv.dataSource = self
+        return cv
+    }()
     
     let colorArr = [#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), .red, #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1), #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1), #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1), #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1), #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)]
     
@@ -22,6 +33,7 @@ class CanvasController : UIViewController {
         setDoneButton()
         setUndoButton()
         setTableView()
+        setupToolCollectionView()
     }
     
     func setBackgroundColor() {
@@ -53,6 +65,16 @@ class CanvasController : UIViewController {
         tableView.dataSource = self
         tableView.pin(view: colorButton)
     }
+    
+    func setupToolCollectionView() {
+        view.addSubview(toolCollection)
+        NSLayoutConstraint.activate([
+            toolCollection.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            toolCollection.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            toolCollection.heightAnchor.constraint(equalToConstant: 100),
+            toolCollection.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
 
 extension CanvasController : UITableViewDelegate, UITableViewDataSource {
@@ -74,7 +96,6 @@ extension CanvasController : UITableViewDelegate, UITableViewDataSource {
         colorButton.backgroundColor = colorArr[indexPath.row]
         tableView.isHidden = true
     }
-    
 }
 
 extension CanvasController {
@@ -92,5 +113,17 @@ extension CanvasController {
     @objc
     func undoAction() {
         
+    }
+}
+
+extension CanvasController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = toolCollection.dequeueReusableCell(withReuseIdentifier: ToolCell.id, for: indexPath) as! ToolCell
+        cell.backgroundColor = .brown
+        return cell
     }
 }
