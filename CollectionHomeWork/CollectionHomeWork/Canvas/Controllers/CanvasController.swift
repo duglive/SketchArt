24eabfbf7c1c47bccdf2e67ace59fc8d686ccd40
duglive <duglive@ya.ru>
2021-06-05 22:16:51 +0300

    add draw tools

--- CollectionHomeWork/CollectionHomeWork/Canvas/Controllers/CanvasController.swift
+++ CollectionHomeWork/CollectionHomeWork/Canvas/Controllers/CanvasController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 final class CanvasController : UIViewController {
-    
     var tableView = ColorTableView()
     let colorButton = ColorButton()
+    let drawView = DrawView()
+    
+    lazy var panGesture: UIPanGestureRecognizer = {
+        let gesture = UIPanGestureRecognizer()
+        gesture.addTarget(self, action: #selector(drawFigur(_:)))
+        return gesture
+    }()
     
     private lazy var toolsCollectionView: ToolsCollectionView = {
         let view = ToolsCollectionView(with: self)
@@ -18,19 +24,20 @@ final class CanvasController : UIViewController {
         return view
     }()
     
-    var currentSelectedTool: ToolType = ToolType.line
-    let tools = [ToolType.square, ToolType.roundedSquare, ToolType.ellipse, ToolType.line, ToolType.triangle, ToolType.straightLine]
+    var selectedTool: ToolType = ToolType.line
+    let tools = [ToolType.square, .roundedSquare, .ellipse, .line, .triangle, .straightLine]
     let colorArr = [#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), .red, #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1), #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1), #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1), #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1), #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
+        setupViews()
     }
 }
 
 extension CanvasController {
-    func setupView() {
+    func setupViews() {
         setBackgroundColor()
+        setDrawView()
         setColorButton()
         setDoneButton()
         setUndoButton()
@@ -40,6 +47,11 @@ extension CanvasController {
     
     func setBackgroundColor() {
         view.backgroundColor = .white
+    }
+    
+    func setDrawView() {
+        drawView.pin(view: self.view)
+        drawView.addGestureRecognizer(panGesture)
     }
     
     func setColorButton() {
@@ -88,7 +100,7 @@ extension CanvasController {
 
 extension CanvasController : UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        16
+        colorArr.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -122,11 +134,47 @@ extension CanvasController {
     func undoAction() {
         
     }
+    
+    @objc
+    func drawFigur(_ gestureRecognizer: UIPanGestureRecognizer) {
+        guard gestureRecognizer.view != nil else {return}
+        
+        if gestureRecognizer.state == .began {
+            drawView.startPoint = gestureRecognizer.location(in: drawView)
+        }
+        
+        if gestureRecognizer.state == .changed {
+            if selectedTool == .line {
+                let currentPoint = gestureRecognizer.location(in: drawView)
+                drawView.drawLine(drawView.startPoint, currentPoint)
+                drawView.startPoint = currentPoint
+            }
+        }
+        
+        if gestureRecognizer.state == .ended {
+            drawView.endPoint = gestureRecognizer.location(in: drawView)
+            
+            switch selectedTool {
+            case .ellipse:
+                drawView.drawElipse(drawView.startPoint, drawView.endPoint)
+            case .square:
+                drawView.drawSquare(drawView.startPoint, drawView.endPoint)
+            case .triangle:
+                drawView.drawTriangle(drawView.startPoint, drawView.endPoint)
+            case .roundedSquare:
+                drawView.drawRoundedSquare(drawView.startPoint, drawView.endPoint)
+            case .line:
+                drawView.drawLine(drawView.startPoint, drawView.startPoint)
+            case .straightLine:
+                drawView.drawStraightLine(drawView.startPoint, drawView.endPoint)
+            }
+        }
+    }
 }
 
 extension CanvasController: ToolsViewDelegate {
     func didSelectTool(at index: IndexPath) {
-        currentSelectedTool = tools[index.row]
+        selectedTool = tools[index.row]
     }
     
     func item(at index: IndexPath) -> ToolType {
